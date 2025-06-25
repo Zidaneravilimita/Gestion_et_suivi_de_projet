67591dba3a2015944644743231c1c0d97a61edb2
Zidane <zidaneravilimita@gmail.com>
2025-06-25 14:24:24 +0300

    configuration de firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router'; // Importez le routeur ici
+import router from './router'; 
 
 createApp(App)
-  .use(router) // Ajoutez le routeur à votre application
+  .use(router) 
   .mount('#app');
