f167b8dc2c42a1b1450d13bd81e45d1766d5a9ba
Zidane <zidaneravilimita@gmail.com>
2025-07-04 00:22:58 +0300

    creation du footer

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
-
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 const app = createApp(App)
 app.use(router)
